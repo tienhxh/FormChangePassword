aa10fa2814e0342b4318f612b2a57acae2b7ec97
Quach Thai Duong <duong.qt210253@sis.hust.edu.vn>
2023-12-27 21:22:18 +0700

    hehe

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,8 +34,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -43,10 +43,11 @@
             // txtMatKhauMoi
             // 
             this.txtMatKhauMoi.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtMatKhauMoi.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMatKhauMoi.Location = new System.Drawing.Point(123, 131);
+            this.txtMatKhauMoi.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhauMoi.Location = new System.Drawing.Point(164, 161);
+            this.txtMatKhauMoi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMatKhauMoi.Name = "txtMatKhauMoi";
-            this.txtMatKhauMoi.Size = new System.Drawing.Size(345, 33);
+            this.txtMatKhauMoi.Size = new System.Drawing.Size(459, 40);
             this.txtMatKhauMoi.TabIndex = 1;
             this.txtMatKhauMoi.Enter += new System.EventHandler(this.txtMatKhauMoi_Enter);
             this.txtMatKhauMoi.Leave += new System.EventHandler(this.txtMatKhauMoi_Leave);
@@ -54,10 +55,11 @@
             // txtMatKhauCu
             // 
             this.txtMatKhauCu.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtMatKhauCu.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMatKhauCu.Location = new System.Drawing.Point(123, 81);
+            this.txtMatKhauCu.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhauCu.Location = new System.Drawing.Point(164, 100);
+            this.txtMatKhauCu.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMatKhauCu.Name = "txtMatKhauCu";
-            this.txtMatKhauCu.Size = new System.Drawing.Size(345, 32);
+            this.txtMatKhauCu.Size = new System.Drawing.Size(459, 38);
             this.txtMatKhauCu.TabIndex = 2;
             this.txtMatKhauCu.Enter += new System.EventHandler(this.txtMatKhauCu_Enter);
             this.txtMatKhauCu.Leave += new System.EventHandler(this.txtMatKhauCu_Leave);
@@ -65,22 +67,24 @@
             // txtNhapLai
             // 
             this.txtNhapLai.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.txtNhapLai.Font = new System.Drawing.Font("Century Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNhapLai.Location = new System.Drawing.Point(123, 185);
+            this.txtNhapLai.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNhapLai.Location = new System.Drawing.Point(164, 228);
+            this.txtNhapLai.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNhapLai.Name = "txtNhapLai";
-            this.txtNhapLai.Size = new System.Drawing.Size(345, 32);
+            this.txtNhapLai.Size = new System.Drawing.Size(459, 38);
             this.txtNhapLai.TabIndex = 3;
             this.txtNhapLai.Enter += new System.EventHandler(this.txtNhapLai_Enter);
             this.txtNhapLai.Leave += new System.EventHandler(this.txtNhapLai_Leave);
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.PeachPuff;
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
             this.button1.Font = new System.Drawing.Font("Bahnschrift", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.IndianRed;
-            this.button1.Location = new System.Drawing.Point(123, 229);
+            this.button1.ForeColor = System.Drawing.Color.Lavender;
+            this.button1.Location = new System.Drawing.Point(164, 282);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 46);
+            this.button1.Size = new System.Drawing.Size(176, 57);
             this.button1.TabIndex = 4;
             this.button1.Text = "Xác nhận";
             this.button1.UseVisualStyleBackColor = false;
@@ -88,9 +92,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::frm.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(21, 26);
+            this.pictureBox1.Location = new System.Drawing.Point(28, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(64, 85);
+            this.pictureBox1.Size = new System.Drawing.Size(85, 105);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -105,40 +110,44 @@
             this.panel1.Controls.Add(this.txtMatKhauCu);
             this.panel1.Controls.Add(this.txtMatKhauMoi);
             this.panel1.Controls.Add(this.txtNhapLai);
-            this.panel1.Location = new System.Drawing.Point(53, 22);
+            this.panel1.Location = new System.Drawing.Point(71, 27);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(700, 364);
+            this.panel1.Size = new System.Drawing.Size(933, 448);
             this.panel1.TabIndex = 6;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(317, 39);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "THAY ĐỔI MẬT KHẨU";
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.PeachPuff;
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
             this.button3.Font = new System.Drawing.Font("Bahnschrift", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.Color.IndianRed;
-            this.button3.Location = new System.Drawing.Point(336, 229);
+            this.button3.ForeColor = System.Drawing.Color.Lavender;
+            this.button3.Location = new System.Drawing.Point(448, 282);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(132, 46);
+            this.button3.Size = new System.Drawing.Size(176, 57);
             this.button3.TabIndex = 8;
             this.button3.Text = "Quay lại";
             this.button3.UseVisualStyleBackColor = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 49);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "THAY ĐỔI MẬT KHẨU";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::frm.Properties.Resources.images;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
